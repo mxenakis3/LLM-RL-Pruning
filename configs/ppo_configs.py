f7538f7a3f380f8c75eed8ad7c6bfab66572c45d
Matheo Xenakis <mxenakis3@atl1-1-03-012-28-0.pace.gatech.edu>
2025-04-28 16:15:48 -0400

    Notebook

--- configs/ppo_configs.py
+++ configs/ppo_configs.py
@@ -9,7 +9,25 @@ texas_holdem_config = Config({
 "continuous": False, # Discrete, or continuous simulation -- OpenAI parameter})
 })
 
-ppo_interaction_config_texas = Config({
+ppo_interaction_config_texas_ex = Config({
+  # INTERACTION CONFIGURATIONS:
+"training_episodes": 10,
+"testing_episodes": 2,
+"bot_type": "random", # "heuristic" or "random"
+"c": 0.15, # Loss function clipping coefficient
+"batch_size":128
+, # Size of batch for SGD in update method.
+"capacity": 20000, # Size of experience tuple (s, a, r, s_, ...) memory
+"num_epochs": 4, # number of batches in each iteration of learning
+"update_frequency": 1000, # how many steps are taken before an update occurs
+"kap_start" : 0.5, # The probability of samping an action from the LLM agent at the first timestep
+"kap_end": 0.001, # The min probability of sampling an action from the LLM agent
+"kap_decay_episodes": 10, # For linear decay: The number of steps we are taking to decay kap. Each episode, kap decays by (kap_start - kap_finish)/ kap_decay_episodes.
+"kap_decay_rate": 0.98, # For exponential decay: The rate of decay for kap, the probability that we sample from llm. kap = kap_start* kap_decay_rate^(episode_number)
+"kap_decay_type": "linear", # Exponential or linear
+})
+
+ppo_interaction_config_texas_random = Config({
   # INTERACTION CONFIGURATIONS:
 "training_episodes": 70000,
 "testing_episodes": 3000,
@@ -22,11 +40,28 @@ ppo_interaction_config_texas = Config({
 "update_frequency": 1000, # how many steps are taken before an update occurs
 "kap_start" : 0.5, # The probability of samping an action from the LLM agent at the first timestep
 "kap_end": 0.001, # The min probability of sampling an action from the LLM agent
-"kap_decay_episodes": 8, # For linear decay: The number of steps we are taking to decay kap. Each episode, kap decays by (kap_start - kap_finish)/ kap_decay_episodes.
+"kap_decay_episodes": 1000, # For linear decay: The number of steps we are taking to decay kap. Each episode, kap decays by (kap_start - kap_finish)/ kap_decay_episodes.
 "kap_decay_rate": 0.98, # For exponential decay: The rate of decay for kap, the probability that we sample from llm. kap = kap_start* kap_decay_rate^(episode_number)
 "kap_decay_type": "linear", # Exponential or linear
 })
 
+ppo_interaction_config_texas_heuristic = Config({
+  # INTERACTION CONFIGURATIONS:
+"training_episodes": 70000,
+"testing_episodes": 3000,
+"bot_type": "heuristic", # "heuristic" or "random"
+"c": 0.3, # Loss function clipping coefficient
+"batch_size":64
+, # Size of batch for SGD in update method.
+"capacity": 20000, # Size of experience tuple (s, a, r, s_, ...) memory
+"num_epochs": 4, # number of batches in each iteration of learning
+"update_frequency": 1000, # how many steps are taken before an update occurs
+"kap_start" : 0.5, # The probability of samping an action from the LLM agent at the first timestep
+"kap_end": 0.001, # The min probability of sampling an action from the LLM agent
+"kap_decay_episodes": 1000, # For linear decay: The number of steps we are taking to decay kap. Each episode, kap decays by (kap_start - kap_finish)/ kap_decay_episodes.
+"kap_decay_rate": 0.98, # For exponential decay: The rate of decay for kap, the probability that we sample from llm. kap = kap_start* kap_decay_rate^(episode_number)
+"kap_decay_type": "linear", # Exponential or linear
+})
 actor_configs_texas = Config({
 # AGENT CONFIGURATIONS
 "learning_rate": 3e-5,
@@ -41,7 +76,38 @@ actor_configs_texas = Config({
 "gradient_clipping": 0.5
 })
 
+actor_configs_texas_heuristic = Config({
+# AGENT CONFIGURATIONS
+"learning_rate": 3e-5,
+"loss_function": "mseloss",
+"optimizer": "adam",
+"gamma": 0.99, # Discount factor
+"obs_size": 76, # dimension of observation
+"output_size": 4, # dimension of action space
+"hidden_layer_sizes": [256, 256],
+"activations": ["ReLU", "ReLU"],
+"is_actor": True, # Leave as True: distinguishes actor and critic networks
+"gradient_clipping": 0.5
+})
+
 critic_configs_texas = Config({
+# INTERACTION CONFIGURATIONS:
+
+# AGENT CONFIGURATIONS
+"learning_rate": 0.001,
+"loss_function": "mseloss",
+"optimizer": "adam",
+"gamma": 1.0, # Discount factor
+"lam":0.95, # Exponential weight for GAE. Probably leave at 0.95
+"obs_size": 76, # dimension of observation
+"output_size": 1, # scalar output
+"hidden_layer_sizes": [256, 256],
+"activations": ["ReLU", "ReLU"],
+"is_actor": False, # Leave as false: distinguishes actor and critic networks,
+"gradient_clipping": 0.5
+})
+
+critic_configs_texas_heuristic = Config({
 # INTERACTION CONFIGURATIONS:
 
 # AGENT CONFIGURATIONS
