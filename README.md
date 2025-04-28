5e9b277fd14a6b7f95305d6baa956e9ebd657d50
Nick Wong <nickwong@ipsec-10-2-192-222.vpn.gatech.edu>
2025-04-28 14:35:42 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# LLM-RL-Pruning
-Pruning RL state space using LLM
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#instructions"> Instructions </a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+        <li><a href="#running-the-code">Running the Code</a></li>
+      </ul>
+    </li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## Instructions
+CS7750 Project: Accelerating Deep Multi-Agent Reinforcement Learning with LLM-Agents
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- Instructions -->
+## Getting Started
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+This is an example of how to list things you need to use the software and how to install them.
+* npm
+  ```sh
+  npm install npm@latest -g
+  ```
+
+### Installation
+1. Clone the repo
+   ```sh
+   git clone https://github.com/mxenakis3/LLM-RL-Pruning.git
+   ```
+3. Install python packages 
+   ```sh
+   pip install -r requirements.txt
+   ```
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Running the code
+The code to generate results is available under /notebooks, and configurations can be changed under /configs
--- configs/ppo_configs_dict.py
+++ configs/ppo_configs_dict.py
@@ -102,3 +102,50 @@ critic_configs_texas_opt = {
 "is_actor": False, # Leave as false: distinguishes actor and critic networks,
 "gradient_clipping": 0.5
 }
+ppo_interaction_config_texas_random_opt = {
+  # INTERACTION CONFIGURATIONS:
+"training_episodes": 100000,
+"testing_episodes": 5000,
+"bot_type": "random", # "heuristic" or "random"
+"c": 0.15, # Loss function clipping coefficient
+"batch_size":128, # Size of batch for SGD in update method.
+"capacity": 20000, # Size of experience tuple (s, a, r, s_, ...) memory
+"num_epochs": 4, # number of batches in each iteration of learning
+"update_frequency": 1000, # how many steps are taken before an update occurs
+"kap_start" : 1.0, # The probability of samping an action from the LLM agent at the first timestep
+"kap_end": 0.001, # The min probability of sampling an action from the LLM agent
+"kap_decay_episodes": 3, # For linear decay: The number of steps we are taking to decay kap. Each episode, kap decays by (kap_start - kap_finish)/ kap_decay_episodes.
+"kap_decay_rate": 0.98, # For exponential decay: The rate of decay for kap, the probability that we sample from llm. kap = kap_start* kap_decay_rate^(episode_number)
+"kap_decay_type": "linear", # Exponential or linear
+}
+
+actor_configs_texas_random_opt = {
+# AGENT CONFIGURATIONS
+"learning_rate": 3e-5,
+"loss_function": "mseloss",
+"optimizer": "adam",
+"gamma": 0.99, # Discount factor
+"obs_size": 76, # dimension of observation
+"output_size": 4, # dimension of action space
+"hidden_layer_sizes": [256, 256],
+"activations": ["ReLU", "ReLU"],
+"is_actor": True, # Leave as True: distinguishes actor and critic networks
+"gradient_clipping": 0.5
+}
+
+critic_configs_texas_random_opt = {
+# INTERACTION CONFIGURATIONS:
+
+# AGENT CONFIGURATIONS
+"learning_rate": 3e-4,
+"loss_function": "mseloss",
+"optimizer": "adam",
+"gamma": 1.0, # Discount factor
+"lam":0.95, # Exponential weight for GAE. Probably leave at 0.95
+"obs_size": 76, # dimension of observation
+"output_size": 1, # scalar output
+"hidden_layer_sizes": [256, 256],
+"activations": ["ReLU", "ReLU"],
+"is_actor": False, # Leave as false: distinguishes actor and critic networks,
+"gradient_clipping": 0.5
+}
